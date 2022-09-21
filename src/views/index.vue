af9bdca3a0929f3c84773803f1be36a024410964
shanghai-zhoupeihuang <zhoupeihuang@sinopharm.com>
2022-09-21 16:53:56 +0800

    '初始化项目'

---
+++ src/views/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app-container home center">
+    <h1 style="font-size: 36px">欢迎使用<strong>运输调度平台</strong></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.center{
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  display: -webkit-box;
+  height: 500px;
+
+}
+</style>
