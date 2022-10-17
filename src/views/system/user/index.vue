6ad157025e7b11ca9aa29cbb0be2fd1916885d63
上海.周佩煌 <zhoupeihuang@sinopharm.com>
2022-10-17 13:52:29 +0800

    '部门和字典管理的修复'

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -138,8 +138,8 @@
         <el-table v-loading="loading" :data="userList" @selection-change="handleSelectionChange">
           <el-table-column type="selection" width="50" align="center" />
           <el-table-column label="用户编号" align="center" key="userId" prop="userId" v-if="columns[0].visible" />
-          <el-table-column label="用户名称" align="center" key="userName" prop="userName" v-if="columns[1].visible" :show-overflow-tooltip="true" />
-          <el-table-column label="用户昵称" align="center" key="nickName" prop="nickName" v-if="columns[2].visible" :show-overflow-tooltip="true" />
+          <el-table-column label="账号" align="center" key="userName" prop="userName" v-if="columns[1].visible" :show-overflow-tooltip="true" />
+          <el-table-column label="用户名称" align="center" key="nickName" prop="nickName" v-if="columns[2].visible" :show-overflow-tooltip="true" />
           <el-table-column label="部门" align="center" key="deptName" prop="dept.deptName" v-if="columns[3].visible" :show-overflow-tooltip="true" />
           <el-table-column label="手机号码" align="center" key="phonenumber" prop="phonenumber" v-if="columns[4].visible" width="120" />
           <el-table-column label="状态" align="center" key="status" v-if="columns[5].visible">
@@ -207,11 +207,10 @@
     <el-dialog :title="title" :visible.sync="open" width="600px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="120px">
         <el-row>
-         
-            <el-col :span="12">
-          <!-- v-if="form.userId == undefined" -->
-            <el-form-item  label="用户名称" prop="userName">
-              <el-input v-model="form.userName" placeholder="请输入用户(手机号)" maxlength="30" />
+          <el-col :span="12">
+           
+              <el-form-item v-if="form.userId == undefined" label="账号" prop="userName">
+              <el-input v-model="form.userName" placeholder="请输入账号" maxlength="30" />
             </el-form-item>
           </el-col>
           <el-col :span="12">
@@ -221,14 +220,29 @@
           </el-col>
         </el-row>
         <el-row>
-        <!-- <el-col :span="12">
+          <el-col :span="12">
             <el-form-item label="手机号码" prop="phonenumber">
               <el-input v-model="form.phonenumber" placeholder="请输入手机号码" maxlength="11" />
             </el-form-item>
-          </el-col> -->
-              <el-col :span="12">
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="邮箱" prop="email">
+              <el-input v-model="form.email" placeholder="请输入邮箱" maxlength="50" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12">
+           <el-form-item label="用户名称" prop="nickName">
+              <el-input v-model="form.nickName" placeholder="请输入用户名称" maxlength="30" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <!-- <el-form-item v-if="form.userId == undefined" label="用户密码" prop="password">
+              <el-input v-model="form.password" placeholder="请输入用户密码" type="password" maxlength="20" show-password/>
+            </el-form-item> -->
             <el-form-item label="用户性别" prop="sex">
-              <el-select v-model="form.sex"  placeholder="请选择性别">
+              <el-select v-model="form.sex" placeholder="请选择性别">
                 <el-option
                   v-for="dict in dict.type.sys_user_sex"
                   :key="dict.value"
@@ -236,6 +250,40 @@
                   :value="dict.value"
                 ></el-option>
               </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="24">
+           
+          
+            
+             <el-form-item label="区域-职位-级别">
+              <el-select v-model="form.postId" filterable   placeholder="请选择区域-职位-级别">
+                <el-option
+                  v-for="item in postOptions"
+                  :key="item.postId"
+                  :label="item.postName"
+                  :value="item.postId"
+                  :disabled="item.status == 1"
+                >
+                  <!-- <span style="float: left">{{ item.regionName+'-'+item.postName+'-'+item.postLevel }}</span> -->
+                 <!-- <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span> -->
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12">
+           <el-form-item label="状态">
+              <el-radio-group v-model="form.status">
+                <el-radio
+                  v-for="dict in dict.type.sys_normal_disable"
+                  :key="dict.value"
+                  :label="dict.value"
+                >{{dict.label}}</el-radio>
+              </el-radio-group>
             </el-form-item>
           </el-col>
           <el-col :span="12">
@@ -252,44 +300,6 @@
               </el-select>
             </el-form-item>
           </el-col>
-        </el-row>
-        
-        <el-row>
-           <el-col :span="12">
-            <el-form-item label="邮箱" prop="email">
-              <el-input v-model="form.email" placeholder="请输入邮箱" maxlength="50" />
-            </el-form-item>
-          </el-col>
-          <el-col :span="12">
-            <el-form-item label="状态">
-              <el-radio-group v-model="form.status">
-                <el-radio
-                  v-for="dict in dict.type.sys_normal_disable"
-                  :key="dict.value"
-                  :label="dict.value"
-                >{{dict.label}}</el-radio>
-              </el-radio-group>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row>
-          <el-col :span="24">
-            <el-form-item label="区域-职位-级别">
-              <el-select v-model="form.postId" filterable   placeholder="请选择区域-职位-级别">
-                <el-option
-                  v-for="item in postOptions"
-                  :key="item.postId"
-                  :label="item.postName"
-                  :value="item.postId"
-                  :disabled="item.status == 1"
-                >
-                  <!-- <span style="float: left">{{ item.regionName+'-'+item.postName+'-'+item.postLevel }}</span> -->
-                 <!-- <span style="float: right; color: #8492a6; font-size: 13px">{{ item.value }}</span> -->
-                </el-option>
-              </el-select>
-            </el-form-item>
-          </el-col>
-          
         </el-row>
         <el-row>
           <el-col :span="24">
@@ -430,14 +440,17 @@ export default {
         userName: [
           { required: true, message: "用户名称不能为空", trigger: "blur" },          
           // { min: 2, max: 20, message: '用户名称长度必须介于 2 和 20 之间', trigger: 'blur' }
-           {
-            pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/,
-            message: "请输入正确的手机号码",
-            trigger: "blur"
-          }
+          //  {
+          //   pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/,
+          //   message: "请输入正确的手机号码",
+          //   trigger: "blur"
+          // }
+        ],
+        deptId: [
+          { required: true, message: "归属部门不能为空", trigger: "change" },        
         ],
         sex: [
-          { required: true, message: "性别不能为空", trigger: "blur" },        
+          { required: true, message: "性别不能为空", trigger: "change" },        
         ],
         password: [
           { required: true, message: "用户密码不能为空", trigger: "blur" },
@@ -450,14 +463,14 @@ export default {
             trigger: ["blur", "change"]
           }
         ],
-        // phonenumber: [
-        //   { required: true, message: "手机号码不能为空", trigger: "blur" },
-        //   {
-        //     pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/,
-        //     message: "请输入正确的手机号码",
-        //     trigger: "blur"
-        //   }
-        // ]
+        phonenumber: [
+          { required: true, message: "手机号码不能为空", trigger: "blur" },
+          {
+            pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/,
+            message: "请输入正确的手机号码",
+            trigger: "blur"
+          }
+        ]
       }
     };
   },
