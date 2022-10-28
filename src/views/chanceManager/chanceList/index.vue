017f495fffab2afde262fb2238e07e0ddb777bd4
上海.周佩煌 <zhoupeihuang@sinopharm.com>
2022-10-28 11:58:21 +0800

    '更新插件'

--- src/views/chanceManager/chanceList/index.vue
+++ src/views/chanceManager/chanceList/index.vue
@@ -1,50 +1,45 @@
 <template>
   <div class="app-container">
- <div class="searchBox">
-        <el-form ref="form" :model="searchForm" label-width="60px" inline>
-           <el-row >
-            <el-col :span="6" style="text-align:left" >
-              机会列表
-            </el-col>
-             <el-col :offset="8" :span="4" >
-              <el-form-item label="搜索">
-                <el-input
-                  v-model="searchForm.chanceName"
-                  @change="init()"
-                  placeholder="请输入机会名称"
-                  clearable
-                ></el-input>
-              </el-form-item>
-              </el-col>
-             <el-col   :span="4" >
-              <el-form-item label="状态">
-                <el-select
-                  v-model="searchForm.chanceStatus"
-                  @change="init()"
-                  placeholder="请选择"
+    <div class="searchBox">
+      <el-form ref="form" :model="searchForm" label-width="60px" inline>
+        <el-row>
+          <el-col :span="6" style="text-align: left"> 机会列表 </el-col>
+          <el-col :offset="8" :span="4">
+            <el-form-item label="搜索">
+              <el-input
+                v-model="searchForm.chanceName"
+                @change="init()"
+                placeholder="请输入机会名称"
+                clearable
+              ></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="4">
+            <el-form-item label="状态">
+              <el-select
+                v-model="searchForm.chanceStatus"
+                @change="init()"
+                placeholder="请选择"
+              >
+                <el-option
+                  v-for="item in chanceStatusOptions"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
                 >
-                  <el-option
-                    v-for="item in chanceStatusOptions"
-                    :key="item.value"
-                    :label="item.label"
-                    :value="item.value"
-                  >
-                    <span :class="[item.value == 4 ? 'color4' : '']">{{
-                      item.label
-                    }}</span>
-                  </el-option>
-                </el-select>
-              </el-form-item>
-            </el-col>
-             <el-col :span="2" >
-                <el-button type="primary" @click="addChance">新增机会</el-button>
-               </el-col>
-           </el-row>
-           
-        </el-form>
-      </div>
-      <!-- <div class="hr"></div> -->
-      <div class="pageTitle cls">
+                  <span :class="[item.value == 4 ? 'color4' : '']">{{ item.label }}</span>
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="2">
+            <el-button type="primary" @click="addChance">新增机会</el-button>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+    <!-- <div class="hr"></div> -->
+    <div class="pageTitle cls">
       <el-table
         :data="tableData"
         class="myTable"
@@ -52,110 +47,89 @@
         style="width: 100%"
         max-height="650"
       >
-        <el-table-column
-          prop="chanceName"
-          fixed
-          label="机会名称"
-          min-width="120"
-        >
+        <el-table-column prop="chanceName" fixed label="机会名称" min-width="120">
           <template slot-scope="scope">
-            <span :class="[scope.row.isNew==1?'isNew':'']"></span>
-            <span :class="['yuan','yuan'+scope.row.priority]"></span>
+            <span :class="[scope.row.isNew == 1 ? 'isNew' : '']"></span>
+            <span :class="['yuan', 'yuan' + scope.row.priority]"></span>
             {{ scope.row.chanceName }}
-           
           </template>
         </el-table-column>
-          <el-table-column
-          prop="chanceName"
-          fixed
-          label="状态"
-          width="120"
-        >
-         </el-table-column>
-          <el-table-column
-          prop="chanceName"
-          label="已转项目"
-        >
-         </el-table-column>
-          <el-table-column
-          prop="chanceName"
-          label="负责人"
-          width="120"
-        >
-         </el-table-column>
-          <el-table-column
-          prop="chanceName"
-          label="客户"
-        >
-         </el-table-column>
-          <el-table-column
-          prop="chanceName"
-          label="客户联系人"
-        >
-         </el-table-column>
-          <el-table-column
-          prop="chanceName"
-          label="客户预算"
-        >
-         </el-table-column>
-          <el-table-column
-          prop="chanceName"
-          label="成功率"
-          width="120"
-        >
-         </el-table-column>
-          <el-table-column
-          prop="chanceName"
-          label="预计开始日期"
-        >
-         </el-table-column>
-          <el-table-column
-          prop="chanceName"
-          label="估算工时(人日)"
-        >
-         </el-table-column>
-          <el-table-column
-          prop="chanceName"
-          label="估计报价"
-        >
-         </el-table-column>
-         <el-table-column
-          prop="chanceName"
-          label="备注"
-        >
-         </el-table-column>
+        <el-table-column prop="chanceStatus" fixed label="状态" width="120">
+        </el-table-column>
+        <el-table-column prop="chanceName" label="已转项目"> </el-table-column>
+        <el-table-column prop="chanceUserName" label="负责人" width="120"> </el-table-column>
+        <el-table-column prop="chanceName" label="客户"> </el-table-column>
+        <el-table-column prop="chanceName" label="客户联系人"> </el-table-column>
+        <el-table-column prop="chanceName" label="客户预算"> </el-table-column>
+        <el-table-column prop="chanceName" label="成功率" width="120"> </el-table-column>
+        <el-table-column prop="chanceName" label="预计开始日期"> </el-table-column>
+        <el-table-column prop="chanceName" label="估算工时(人日)"> </el-table-column>
+        <el-table-column prop="chanceName" label="估计报价"> </el-table-column>
+        <el-table-column prop="chanceName" label="备注"> </el-table-column>
       </el-table>
     </div>
   </div>
 </template>
 <script>
- 
-
 export default {
   data() {
     return {
-      tableData: [],
-         chanceStatusOptions: [
+      tableData: [
         {
-          label: "全部",
-          value: '',
+          chanceName: "", //          机会名称
+          chanceStatus: "", //          机会状态：1.新建、2.跟进中、3.已结束、4.已转项目
+          priority: "", //          优先级（1.最高;2.高，3.普通，4.较低）
+          projectId:null, //      已转项目
+          projectName:null, //      已转项目
+          chanceUserId: "",//       机会负责人id
+          chanceUserName:"", //          机会负责人
+          customer: "", //          客户
+          customerLink: "", //          客户联系人
+          tel: "", //          联系方式
+          budget: "", //          客户预算
+          successRate: "", //          成功率
+          expectStartTime: "", //          预计开始时间
+          expectEndTime: "", //          预计结束时间
+          expectWork: "", //          预计工时（人日）
+          expectOffer: "", //          预计报价
+          chanceService: "", //          服务对象(1.对内;2.对外)
+          remark: "", //          备注
         },
+      ],
+
+      chanceConfigList: [
         {
-          label: "新建",
-          value: 1,
+          postId: "", //职位id
+          areaId: "", //区域id
+          gradeId: "", //等级id
+          skillIdList: "", //技能id
+          startTime: "", //开始时间
+          endTime: "", //结束时间
+          workTime: "", //总工时
+          workDay: "", //总人日（工作日*工时/8）
+          planLoad: "", //计划负荷
+          expectedCost: "", //预计成本
+          chanceConfigScheduleList: [], //
+          week: "", //周数
+          startTime: "", //开始时间
         },
+      ],
+      chanceConfigScheduleList: [
         {
-          label: "跟进中",
-          value: 2,
+          week: "", //周数
+          startTime: "", //开始时间
+          endTime: "", //结束时间
+          workTime: "", //工时
+          weekDay: "", //工作日
+          planLoad: "", //计划负荷
         },
-        {
-          label: "已结束",
-          value: 3,
-        },
-        {
-          label: "已转项目",
-          value: 4,
-        },
+      ],
+      chanceStatusOptions: [
+        { label: "全部", value: "" },
+        { label: "新建",value: 1,},
+        {label: "跟进中",value: 2,},
+        { label: "已结束",value: 3,},
+        { label: "已转项目", value: 4,},
       ], // 机会状态：1.新建、2.跟进中、3.已结束、4.已转项目
       searchForm: {
         // 机会名称
@@ -163,19 +137,18 @@ export default {
         // 机会状态：1.新建、2.跟进中、3.已结束、4.已转项目
         chanceStatus: "",
       },
-     }
+    };
   },
-  mounted() {
-  },
+  mounted() {},
   methods: {
-   init(){
-    console.log("init");
-   },
-   addChance(){
-    // 新增 机会
-    const obj = { path:'/chanceManager/addChance/'};
-    this.$tab.closeOpenPage(obj);
-   }
+    init() {
+      console.log("init");
+    },
+    addChance() {
+      // 新增 机会
+      const obj = { path: "/chanceManager/addChance/" };
+      this.$tab.closeOpenPage(obj);
+    },
   },
 
   beforeCreate() {
@@ -193,10 +166,10 @@ export default {
 .app-container {
   padding: 0;
 }
- .searchBox{
+.searchBox {
   padding: 10px 20px;
   margin-top: 10px;
- }
+}
 .priority4 {
   color: #909399;
 }
@@ -210,4 +183,3 @@ export default {
   color: #f56c6c;
 }
 </style>
- 
