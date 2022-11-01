f11a74cb1307a045f2b44fc93c95aa9cacf54e4f
上海.周佩煌 <zhoupeihuang@sinopharm.com>
2022-11-01 10:06:49 +0800

    '新增机会+项目管理增加人员检索'

--- src/views/chanceManager/chanceDetail/index copy.vue
+++ src/views/chanceManager/chanceDetail/index copy.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="app-container">
-    <div class="routerBar">
-      <router-link :to="'/projectManager/proManager'"> < 新建机会</router-link>
+     <div class="routerBar">
+      <router-link :to="'/chanceManager/chanceList'"> < 机会详情</router-link>
       <div class="rightBox">
-        <el-button size="mini" @click="submitForm" type="primary">保存</el-button>
-        <el-button size="mini" @click="resetForm" type="default">取消</el-button>
+        <router-link :to="'/chanceManager/chanceEdit'"> 编辑基本信息</router-link>
+        <router-link :to="'/chanceManager/addChance'"> < 机会详情</router-link>
+        <span class="priority3" @click="transformProject">转为正式项目</span>
       </div>
     </div>
     <div class="titleBar">机会基础信息</div>
       <div class="whiteBox">
-    <el-form ref="elForm" :model="formData" :rules="rules" size="medium" label-width="110px">
+    <el-form ref="elForm" :model="formData"   size="medium" label-width="110px">
       <el-row  >
         <el-col :span="10" :offset="1">
           <el-form-item label="机会名称：" prop="chanceName">
@@ -178,8 +179,6 @@
         <el-col :span="6">
           <el-form-item label=""  >
             <el-button type="primary" size="mini"  @click="DelConfigList(chanceConfigIndex)"> 删除 </el-button>
-          
-            <!-- <el-button type="info" size="mini"> 取消 </el-button> -->
           </el-form-item>
         </el-col>
       </el-row>
@@ -234,7 +233,7 @@
                   :min="0"
                   :max="24"
                   v-model="chanceConfigScheduleItem.workTime"
-                  :precision='1'
+                   :precision='1'
                   @change="
                     (number) => {
                       changeDayTime(
@@ -246,9 +245,6 @@
                     }
                   " 
                 ></el-input-number>
-                  <!-- 
-                  @input.native="changeInput($event)"
-                 -->
                  小时 
               </div>
         </el-col>
@@ -262,178 +258,43 @@
 
     </el-form>
     </div>
-    </div>
+  </div>
 </template>
 <script>
-import {
+ import {
   queryDict,// 字典查询 传入字典名称 
   addChance,
+  chanceDetail,toProject
 } from "@/api/chanceManager/chanceManager";
 import {
   getTimeProcess,
 } from "@/api/proManager/proManager";
+
 export default {
   data() {
     return {
-      childDateArea:{
-        // 机会配置安排的的 可选时间区间
-        disabledDate: (time) => {
-          if (this.formData.expectStartTime && this.formData.expectEndTime) {
-            // 设置可以选择的区间 时间为项目的 起始日期和结束日期
-            return (
-              time.getTime() > new Date(this.formData.expectEndTime).getTime() ||
-              time.getTime() <
-                new Date(this.formData.expectStartTime).getTime() - 8.64e7
-            );
-          }
-        },
-      },
-      regionOptions:[], // 区域
-      postTypeOptions:[], // 职位
-      gradeIdOptions:[
-        {dictCode:"1",dictLabel:"初级"},
-        {dictCode:"2",dictLabel:"中级"},
-        {dictCode:"3",dictLabel:"高级"},
-      ],  // 等级 后端还未开发
-      techniqueOptions:[], // 技能需求
-      formData: {
-          // chanceName: "", //          机会名称
-          // chanceStatus: "", //          机会状态：1.新建、2.跟进中、3.已结束、4.已转项目
-          // priority: 3, //          优先级（1.最高;2.高，3.普通，4.较低）
-          // projectId:null, //      已转项目
-          // projectName:null, //      已转项目
-          // chanceUserId: "",//       机会负责人id
-          // chanceUserName:"", //          机会负责人
-          // customer: "", //          客户
-          // customerLink: "", //          客户联系人
-          // tel: "", //          联系方式
-          // budget: undefined, //          客户预算
-          // successRate: undefined, //          成功率
-          // expectStartTime: "", //          预计开始时间
-          // expectEndTime: "", //          预计结束时间
-          // expectWork: undefined, //          预计工时（人日）
-          // expectOffer: undefined, //          预计报价
-          // chanceService: "", //          服务对象(1.对内;2.对外)
-          // remark: "", //          备注
-          /*------------测试用-----------------*/
-          chanceName: "机会测试1", 
-          chanceStatus: 1, 
-          priority: 3, 
-          // projectId:null, 
-          // projectName:null, 
-          chanceUserId: 2,
-          // chanceUserName:"测试", 
-          customer: "国药控股合肥奇兵有限公司", 
-          customerLink: "周佩煌", 
-          tel: "13817242305", 
-          budget: 250000, 
-          successRate: 85, 
-          expectStartTime: "2022-10-01", 
-          expectEndTime: "2022-11-20", 
-          expectWork: 66, 
-          expectOffer: 660000, 
-          chanceService: 1, 
-          remark: "都很棒", 
+          formData: {
+          chanceName: "", //          机会名称
+          chanceStatus: "", //          机会状态：1.新建、2.跟进中、3.已结束、4.已转项目
+          priority: 3, //          优先级（1.最高;2.高，3.普通，4.较低）
+          projectId:null, //      已转项目
+          projectName:null, //      已转项目
+          chanceUserId: "",//       机会负责人id
+          chanceUserName:"", //          机会负责人
+          customer: "", //          客户
+          customerLink: "", //          客户联系人
+          tel: "", //          联系方式
+          budget: undefined, //          客户预算
+          successRate: undefined, //          成功率
+          expectStartTime: "", //          预计开始时间
+          expectEndTime: "", //          预计结束时间
+          expectWork: undefined, //          预计工时（人日）
+          expectOffer: undefined, //          预计报价
+          chanceService: "", //          服务对象(1.对内;2.对外)
+          remark: "", //          备注
           chanceConfigList:[]
       },
-      // 配置的基本信息
-      chanceConfigList: 
-      {
-        postId: "", //职位id
-        areaId: "", //区域id
-        gradeId: "", //等级id
-        skillIdList: [], //技能id
-        startTime: "", //开始时间
-        endTime: "", //结束时间
-        workTime: "", //总工时
-        workDay: "", //总人日（工作日*工时/8）
-        planLoad: "", //计划负荷
-        expectedCost: "", //预计成本
-        chanceConfigScheduleList: [], //
-        week: "", //周数
-        startTime: "", //开始时间
-        startEndTime:[],
-        gradeIdActive:true,
-        postTypeActive:true,
-        nextActive:true,
-        chanceConfigScheduleList:[]
-      },
-      chanceConfigScheduleList:{
-        week:"",//周数
-        startTime:"",//开始时间
-        endTime:"",//结束时间
-        workTime:"",//工时
-        weekDay:"",//工作日
-        planLoad:"",//计划负荷
-      },      
-      rules: {
-        chanceName: [{
-          required: true,
-          message: '请输入机会名称',
-          trigger: 'blur'
-        }],
-        chanceStatus: [{
-          required: true,
-          message: '请选择机会状态',
-          trigger: 'change'
-        }],
-        chanceUserId: [{
-          required: true,
-          message: '请输入负责人',
-          trigger: 'change'
-        }],
-        priority: [{
-          required: true,
-          message: '请选择优先级',
-          trigger: 'change'
-        }],
-        customer: [],
-        customerLink: [],
-        tel: [{
-          pattern: /^1(3|4|5|7|8|9)\d{9}$/,
-          message: '请输入正确的手机号！',
-          trigger: 'blur'
-        }],
-        budget: [],
-        successRate: [],
-        expectStartTime: [],
-        expectEndTime: [],
-        expectOffer: [],
-        expectOffer: [],
-        chanceService: [{
-          required: true,
-          message: '请选择对内/对外',
-          trigger: 'change'
-        }],
-        remark: [],
-        // 配置信息的
-        chanceConfigItemAreaId:[{
-          required: true,
-          message: '请选择区域!',
-          trigger: 'change'
-        }],
-        chanceConfigItemPostId:[{
-          required: true,
-          message: '请选择职位!',
-          trigger: 'change'
-        }],
-        chanceConfigItemGradeId:[{
-          required: true,
-          message: '请选择等级!',
-          trigger: 'change'
-        }],
-        chanceConfigItemSkillIdList:[{
-          required: true,
-          message: '请选择技能需求!',
-          trigger: 'change'
-        }],
-        chanceConfigItemStartEndTime:[{
-          required: true,
-          message: '请选择配置时间安排!',
-          trigger: 'change'
-        }],
-      },
-       
+         
       chanceStatusOptions: [{
         "label": "新建",
         "value": 1
@@ -474,187 +335,28 @@ export default {
         "label": "对外",
         "value": 2
       }],
-    }
+     }
   },
   mounted() {
-    this.init();
-    this.getDictList("post_type"); //职位类型
-    this.getDictList("region"); //区域
-    // this.getDictList("gradeId"); // 等级
-    this.getDictList("technique"); // 技能 technique
+    this.init(this.$route.query.chanceId)
   },
   methods: {
-    /*查询字典的接口*/
-    getDictList(dictCode) {
-      queryDict(dictCode).then((res) => {
-        if (dictCode == "post_type") {
-          this.postTypeOptions = res.data;
-        }
-        if (dictCode == "region") {
-          this.regionOptions = res.data;
-        }
-        if (dictCode == "gradeId") {
-          this.gradeIdOptions = res.data;
-        }
-        if (dictCode == "technique") {
-          this.techniqueOptions = res.data;
-        }
-       
-      });
-    },
-    init() {},
-        submitForm() {
-      this.$refs['elForm'].validate(valid => {
-        if (!valid) return
-        // TODO 提交表单
-        console.log(this.formData);
-        
-          let parame = {
-            ...this.formData,
-            // carrierId:this.temData.carrierId,
-            // status:0
-          };
-          addChance(parame).then((res) => {
-            let { code, msg } = res;
+   init(chanceId){
+    chanceDetail(chanceId).then((res)=>{
+      console.log(res.data);
+      this.formData = res.data
+    })
+   },
+   // 转换为正式项目
+   transformProject(){
+    toProject(this.formData.chanceId).then((res)=>{
+         let { code, msg } = res;
             this.$message.success(msg);
             if (+code == 200) {
               this.$router.push("/chanceManager/chanceList");
             }
-          });
-      })
-    },
-    resetForm() {
-      // this.$refs['elForm'].resetFields()
-      // 产品要返回
-      // 新增 机会
-      const obj = { path: "/chanceManager/chanceList" };
-      this.$tab.closeOpenPage(obj);
-    },
-    // 判断当前这个值是否选中了
-    editNext(who,data,index){
-      // console.log(who,data,index);
-      if(who=='region'){
-        this.formData.chanceConfigList[index].postTypeActive = false
-      }
-       if(who=='postType'){
-        this.formData.chanceConfigList[index].gradeIdActive = false
-      }
-       if(who=='gradeId'){
-         this.formData.chanceConfigList[index].nextActive = false
-        // 此处去请求 成本
-        this.formData.chanceConfigList[index].costNum =  1000// 写死成本为 1000
-        // 并计算 下面的周排期
-        this.getTimeArea(this.formData.chanceConfigList[index].startEndTime,index)
-      }
-    },
-     /*根据起始和结束 生成下面表格*/
-    getTimeArea(dates, index) {
-      //  if(this.formData.projectUserList[index].userId==""){
-      //   this.$message.error("请先选择项目成员！");
-      //   return false
-      // }
-      this.constAll(dates, index);
-    },
-    constAll(dates, index) {
-      let params = {
-        startDate: dates[0],
-        endDate: dates[1],
-      };
-      getTimeProcess(params).then((res) => {
-        this.formData.chanceConfigList[index].workDay = res.data.day;
-        this.formData.chanceConfigList[index].workDayTemp = res.data.day;
-        this.formData.chanceConfigList[index].workTime = res.data.day * 8;
-        this.formData.chanceConfigList[index].startTime = dates[0];
-        this.formData.chanceConfigList[index].endTime = dates[1];
-        this.formData.chanceConfigList[index].expectedCost = (
-          res.data.day * this.formData.chanceConfigList[index].costNum
-        ).toFixed(2);
-        if (res.data.day === 0) {
-          this.formData.chanceConfigList[index].planLoad = 0;
-        } else {
-          this.formData.chanceConfigList[index].planLoad = (
-            ((8 * res.data.day) / (res.data.day * 8)) *
-            100
-          ).toFixed(2); // 计划负荷
-        }
-        res.data.list.map((item) => {
-          item.startTime = item.startDate;
-          item.endTime = item.endDate;
-          item.workTime = "8";
-          item.planLoad = (((item.day * 8) / (item.day * 8)) * 100 || 0).toFixed(2);
-        });
-        this.formData.chanceConfigList[index].chanceConfigScheduleList = res.data.list; // 此人的 每周安排
-      });
-      this.$forceUpdate()
-    },
-    /*修改每日工时*/
-    changeDayTime(number, weekDay, fatherIndex, myIndex) {
-      // 期间计划负荷 = 当前行的总天数weekDay*number  /  当前行的总天数weekDay * 8
-      this.formData.chanceConfigList[fatherIndex].chanceConfigScheduleList[
-        myIndex
-      ].planLoad = (((weekDay * number) / (weekDay * 8)) * 100 || 0).toFixed(2);
-      // 循环 取出每周的工作时长
-      let totalTime = 0,
-        totalDay = 0;
-      this.formData.chanceConfigList[fatherIndex].chanceConfigScheduleList.map(
-        (item, i) => {
-          if (myIndex == i) {
-            // 当前周的工时 转换为天数  计算一下
-            // 当前周的天数* 实际工作时长 除以8
-            // 等于 实际工作天数
-
-            totalDay += parseFloat(item.weekDay * number);
-          } else {
-            // 其他的没有修改的 直接 拿天数累加
-            totalDay += parseFloat(item.weekDay * item.workTime);
-          }
-            totalTime += parseFloat(item.workTime) * parseFloat(item.weekDay);
-          // item.weekDay = item.day;
-          // item.week = item?.weekOfYear
-        }
-      );
-      // 暂存一下 实际的天数
-      const tempWorkDay = this.formData.chanceConfigList[fatherIndex].workDayTemp;
-      // 顶部的 共计多少小时  多少天
-      this.formData.chanceConfigList[fatherIndex].workDay = (totalDay / 8).toFixed(2);
-      this.formData.chanceConfigList[fatherIndex].workTime = totalTime.toFixed(2);
-      // 顶部的 计划负荷 预计成本
-      if (totalDay === 0) {
-        this.formData.chanceConfigList[fatherIndex].planLoad = 0;
-      } else {
-        this.formData.chanceConfigList[fatherIndex].planLoad = (
-          (totalDay / (tempWorkDay * 8)) *
-          100
-        ).toFixed(2);
-      }
-      this.formData.chanceConfigList[fatherIndex].expectedCost = (
-        this.formData.chanceConfigList[fatherIndex].workDay *
-        this.formData.chanceConfigList[fatherIndex].costNum
-      ).toFixed(2);
-    },
-    // 删除单行用户
-    DelConfigList(index){
-      this.formData.chanceConfigList.splice(index, 1);
-    },
-    // 点击 新增配置的
-    addConfigListHandel() {
-      //  if(this.formData.projectService==""){
-      //         this.$message.error("请您先完整填写项目基础信息！");
-      //   }
-      this.$refs["elForm"].validate((valid) => {
-        if (!valid) return;
-        // TODO 上面基础信息填写好 再填写下面，因为需要用到上面的 服务对象和项目有效期
-        if (valid) {
-          let oneUser = this.deepClone(this.chanceConfigList);
-          oneUser.startTime = this.formData.expectStartTime;
-          oneUser.endTime = this.formData.expectEndTime;
-          oneUser.startEndTime =[this.formData.expectStartTime, this.formData.expectEndTime]
-
-          this.formData.chanceConfigList.push(oneUser);
-          this.$forceUpdate();
-        }
-      });
-    },
+    })
+   }
   },
 
   beforeCreate() {
@@ -672,15 +374,7 @@ export default {
 .app-container {
   padding: 0;
 }
-.routerBar {
-  a {
-    color: #557db3;
-  }
-  span {
-    font-size: 12px;
-    color: #999;
-  }
-}
+ 
 .priority4 {
   color: #909399;
 }
@@ -693,16 +387,5 @@ export default {
 .priority1 {
   color: #f56c6c;
 }
-.lineTT{
-  // margin-left:40px;
-  font-size:14px;
-  color:#ADB3B9;
-  line-height: 40px;
-}
-.configFont{
-  color:#ADB3B9
-}
-.UserListBox .el-form-item{
-  margin-bottom: 0px;
-}
 </style>
+ 
