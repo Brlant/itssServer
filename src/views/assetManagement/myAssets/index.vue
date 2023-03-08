97a42566794515a3bc20efa3c4567a1278f04140
合肥.郭颖 <3547747324@qq.com>
2023-03-08 18:29:27 +0800

    [change](资产管理)资产借用数据源调整

--- src/views/assetManagement/myAssets/index.vue
+++ src/views/assetManagement/myAssets/index.vue
@@ -171,10 +171,36 @@ export default {
     // 资产类型查询
     getAsset() {
       queryAsset().then(res => {
-        this.asset = res.data
+        // this.asset = res.data
+        let arr = res.data
+        arr.forEach((m) => {
+          if ((m.children ?? '')!=='') {
+            m.children.forEach((item) => {
+              if ((item.children ?? '')=='') {
+                item.disabled = true
+              }
+            });
+          }else if ((m.children ?? '')=='') {
+            m.disabled = true
+          }
+        });
+        this.asset = arr
       })
       queryAssetBorrow().then(res=>{
-        this.assetBorrow = res.data
+        // this.assetBorrow = res.data
+        let arr = res.data
+        arr.forEach((m) => {
+          if ((m.children ?? '') !== '') {
+            m.children.forEach((item) => {
+              if ((item.children ?? '') == '') {
+                item.disabled = true
+              }
+            });
+          }else if ((m.children ?? '') == '') {
+            m.disabled = true
+          }
+        });
+        this.assetBorrow = arr
       })
     },
     // 进入详情页
