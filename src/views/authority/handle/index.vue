be42cc9518e043f85d1b0059341120221f74468c
合肥.郑浩 <3077569207@qq.com>
2023-12-28 22:55:13 +0800

    修复bug

--- src/views/authority/handle/index.vue
+++ src/views/authority/handle/index.vue
@@ -85,13 +85,11 @@
           <span v-if="scope.row.examineStatus === 2" style="color: black">
             审核不通过
           </span>
-          <span v-if="scope.row.examineStatus === 3 && scope.row.modelType === 'inOrder'" style="color: #F79B22">待收货</span>
-          <span v-if="scope.row.examineStatus === 3 && scope.row.modelType !== 'inOrder'" style="color: green">已完成</span>
-          <span v-if="scope.row.examineStatus === 4" style="color: black">
-            已撤回
-          </span>
+<!--          <span v-if="scope.row.examineStatus === 3 && scope.row.modelType === 'inOrder'" style="color: #F79B22">待收货</span>-->
+          <span v-if="scope.row.examineStatus === 3"  style="color: green">已完成</span>
+          <span v-if="scope.row.examineStatus === 4" style="color: black">已撤回</span>
           <span v-if="scope.row.examineStatus === 5" style="color: black">已取消</span>
-          <span v-if="scope.row.examineStatus === 7" style="color: green">已完成</span>
+<!--          <span v-if="scope.row.examineStatus === 7" style="color: green">已完成</span>-->
         </template>
       </el-table-column>>
       <el-table-column label="审核节点" align="center" prop="modelNode"/>
